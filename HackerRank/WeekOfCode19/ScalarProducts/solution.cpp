ba89f122aabd5683dc61842b1b38567bd521dd34
Shun Yao <yaoshun@Shuns-MacBook-Pro.local>
2016-02-22 00:05:06 -0500

    updated

--- HackerRank/WeekOfCode19/ScalarProducts/solution.cpp
+++ HackerRank/WeekOfCode19/ScalarProducts/solution.cpp
@@ -4,67 +4,36 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
-#include <unordered_map>
 using namespace std;
 
+// To even reduce the time complexity.
+// we should use the fibonacci equality.
+// F(n) x F(k) + F(n + 1) x F(k + 1) = F(n + k + 1)
+
 typedef long long ll;
-ll a[300001];
-ll b[300001];
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     ll C, M, n;
     cin >> C >> M >> n;
-    
-    a[0] = 0;
-    b[0] = C;
-    
-    unordered_map<ll, vector<ll>> cset;
 
-    ll cycle = n;
-    
-    for (int i = 1; i <= n; i++) {
-        a[i] = (a[i - 1] + b[i - 1]) % M;
-        b[i] = (b[i - 1] + a[i]) % M;
-        if (cset.find(a[i]) == cset.end()) {
-            cset[a[i]].push_back(b[i]);
-        } else {
-            for (auto p : cset[a[i]]) {
-                if (p == b[i]) {
-                    // cycle detected
-                    // check how many in the cycle can be squared.
-                    cycle = i - 1;
-                    break;
-                }
-            }
-            if (cycle != n) {
-                break;
-            } else {
-                cset[a[i]].push_back(b[i]);
-            }
-        }
-    }
-    
+    // 1<= i, j <= n, i!=j can conclude that 3 <= i + j <= 2n - 1
+    // O(n) time complexity with a hashset.
+    // scalar product of i, j is actually F(2i) x F(2j) + F(2i + 1) x F(2j + 1) = F(2i + 2j + 1) * C
     unordered_set<ll> st;
-    for (int i = 1; i <= min(cycle, n); i++) {
-        for (int j = 1; j < i; j++) {
-            ll temp = (a[i] * a[j]) % M + (b[i] * b[j]) % M;
-            temp %= M;
-            st.insert(temp);
-        }
-    }
-    
-    if (cycle < n) {
-        // Possible squares in the cycle. multiplied with itself in the other position.
-        for (int i = 1; i <= min(n - cycle, cycle); i++) {
-            ll temp = (a[i] * a[i]) % M + (b[i] * b[i]) % M;
-            temp %= M;
-            st.insert(temp);
-        }
+   
+    // 0:0, 1:1, 2:1, 3:2, 4:3, 5:5, 6:8
+    ll a = 5*C%M;
+    ll b = 8*C%M;
+
+    for (int i = 3; i <= 2 * n - 1; i++) {
+        // calculate F(2i + 1) in this case.
+        a = (a + b) % M;
+        st.insert(a * C % M);
+        b = (b + a) % M;
     }
     
     cout << st.size() << endl;
-    
     return 0;
 }
 
