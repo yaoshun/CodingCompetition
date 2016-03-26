8dedd6a1a2555a07d348f1b50b76116e9eceada4
Shun Yao <yaoshun@Shuns-MacBook-Pro.local>
2016-03-26 17:34:42 -0400

    codejam template added

---
+++ GoogleCodeJam/template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cout << "Case #" << (i + 1) << ": " << endl;
+    }    
+    return 0;
+}
