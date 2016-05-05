16f7bda90bb7907b1d6bd25def425aade07686fb
Shun Yao <yaoshun@Shuns-MacBook-Pro.local>
2016-05-05 11:47:05 -0400

    Coding suspendted

---
+++ HackerRank/101HackApril2016/SuperMaximumCostQueries/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+const int P = 1e9 + 7;
+
+// Consider the problem for a single (L,R) pair <=> the problem
+// of only using the edges of Wi <= R, the number of paths in the tree
+// can be calculated from the size of each connected component.
+// solve the problem for <= R and < L can solve the problem.
+
+int main() {
+
+    return 0;
+}
