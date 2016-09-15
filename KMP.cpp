2566599d1b1c8399124798bc20fd5ecfd0e18665
Shun Yao <yaoshun88@gmail.com>
2016-09-15 11:33:16 -0400

    Create KMP.cpp

---
+++ KMP.cpp
@@ -0,0 +1,22 @@
+void KMPlps(const string& s, vector<int>& lps) {
+  int len = 0;
+  int n = s.size();
+  lps.resize(n);
+  lps[0] = 0;
+    
+  int i = 1;
+  while (i < n) {
+    if (s[i] == s[len]) {
+      lps[i] = len + 1;
+      len++;
+      i++;
+    } else {
+      if (len != 0) {
+        len = lps[len - 1];
+      } else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+}
